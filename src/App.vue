a2833910c8e197f21f83e8fe8bf99ec589ea8eeb
youbiaokachi <1281951731@qq.com>
2023-11-23 19:05:20 +0800

    提交

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <lc-button>按钮</lc-button>
-    <lc-button type="primary">按钮</lc-button>
+    <lc-button type="primary">按钮111</lc-button>
     <lc-button size="small" type="success">按钮</lc-button>
     <lc-button size="large" type="success">按钮按钮按钮按钮按钮</lc-button>
     <lc-button type="success">按钮</lc-button>
