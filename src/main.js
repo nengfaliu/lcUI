8c4dc66f1f38c2d0bb5ca3c7117ff9811b65d1d0
youbiaokachi <1281951731@qq.com>
2024-11-28 01:37:33 +0800

    将仓库改成monorepo模式

--- src/main.js
+++
@@ -1,8 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import lcUI from '../widgetPackages/index'
-
-const app = createApp(App)
-app.use(lcUI)
-app.mount('#app')
--- widgetPackages/button/index.js
+++
@@ -1,8 +0,0 @@
-import lcButton from './index.vue'
-
-// 注册
-lcButton.install = app =>{
-    app.component(lcButton.name, lcButton)
-}
-
-export default lcButton
